49cd0944f62138edb1d8ff9e35e33e1ce5a92d74
hisato <st.neco355x@gmail.com>
2015-02-12 19:22:00 +0900

    Ver.1.2.0 - MIT lincense

---
+++ Controller/NginxcacheclearController.php
@@ -0,0 +1,49 @@
+<?php
+  /**
+    * Nginx Cache Clear Controller.
+    *
+    * baserCMS :  Based Website Development Project <http://basercms.net>
+    * Copyright   2008 - 2012, baserCMS Users Community <http://sites.google.com/site/baserusers/>
+    *
+    * @copyright		Copyright 2015, Studio Necomaneki
+    * @link			    http://blog.necomaneki.com/ Studio Necomaneki
+    * @package			NginxCacheClear.Controller
+    * @since			  v 1.2.0
+    * @license      MIT lincense
+    *
+    */
+  App::import('Controller', 'Plugins');
+
+/**
+ * Nginx Cache Clear Controller.
+ */
+class NginxcacheclearController extends BcPluginAppController {
+
+// Controller
+  public $name = 'Nginxcacheclear';
+
+// Component
+  public $components = array('BcAuth', 'Cookie', 'BcAuthConfigure');
+
+// Admin Page Action
+  public function admin_index() {
+
+    App::import('Core', 'Folder');
+    $folder = new Folder(CACHE . Configure::read('Nginxcacheclear.Cachedir') . DS);
+
+    $files = $folder->read(true, true, true, true, true);
+    foreach ($files[1] as $file) {
+      @unlink($file);
+    }
+    $Folder = new Folder();
+    foreach ($files[0] as $folder) {
+      $Folder->delete($folder);
+    }
+
+    $this->setMessage('Nginxキャッシュを削除しました。');
+    $this->redirect($this->referer());
+
+  }
+
+}
+?>
---
+++ config.php
@@ -0,0 +1,23 @@
+<?php
+/**
+  * Nginx Cache Clear Controller.
+  *
+  * baserCMS :  Based Website Development Project <http://basercms.net>
+  * Copyright   2008 - 2012, baserCMS Users Community <http://sites.google.com/site/baserusers/>
+  *
+  * @copyright		Copyright 2015, Studio Necomaneki
+  * @link			    http://blog.necomaneki.com/ Studio Necomaneki
+  * @package		  NginxCacheClear.config
+  * @since			  v 1.2.0
+  * @license      MIT lincense
+  *
+  */
+
+$title = 'Nginxキャッシュクリア';
+$description = 'nginx.confに記述されたキャッシュディレクトリ内の削除。※ディレクトリ名の変更は /Config/setting.php を編集して下さい。';
+$adminLink = array('plugin' => 'nginxcacheclear', 'controller' => 'nginxcacheclear', 'action' => 'index');
+$installMessage = 'NginxCacheClearをインストールしますか？';
+$author = 'hisanuco';
+$url = 'http://blog.necomaneki.com/';
+
+?>
