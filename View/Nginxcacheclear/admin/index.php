e331281de76260ad2557bce8347e72876aa83a24
hisato <st.neco355x@gmail.com>
2015-02-19 20:11:27 +0900

    t12

--- View/Nginxcacheclear/admin/index.php
+++ View/Nginxcacheclear/admin/index.php
@@ -13,7 +13,7 @@
     */
 ?>
 <strong>Nginxcacheclear</strong><br />
-<?php if (!empty($ngxcachedir)): ?>
-<?php echo $ngxcachedir[0]['Nginxcacheclear']['cachedir']; ?>
+<?php if (!$this->ngxcachedir)): ?>
+<?php echo $this->ngxcachedir[0]['Nginxcacheclear']['cachedir']; ?>
 <?php endif; ?>
 <?php echo $this->bcBaser->link('キャッシュディレクトリ名の変更', array('controller'=>'nginxcacheclear', 'action'=>'edit')); ?>
