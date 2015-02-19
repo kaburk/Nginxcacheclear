e49ac8cf99c7ce376bf38ba6f03d905d281be489
hisato <st.neco355x@gmail.com>
2015-02-20 02:51:54 +0900

    edit db

--- Config/sql/nginxcacheclears.php
+++ Config/sql/nginxcacheclears.php
@@ -1,17 +1,4 @@
-<?php
-/**
-  * Nginx Cache Clear Plugin.
-  *
-  * baserCMS :  Based Website Development Project <http://basercms.net>
-  * Copyright   2008 - 2012, baserCMS Users Community <http://sites.google.com/site/baserusers/>
-  *
-  * @copyright		Copyright 2015, Studio Necomaneki
-  * @link			    http://blog.necomaneki.com/ Studio Necomaneki
-  * @since			  v 1.5.0
-  * @license      MIT lincense
-  *
-  */
-
+<?php 
 class NginxcacheclearsSchema extends CakeSchema {
 
 	public $file = 'nginxcacheclears.php';
