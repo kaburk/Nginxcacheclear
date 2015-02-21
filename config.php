2c033f750b4ff206f5ba48be2ca73e2245dd5129
hisato <st.neco355x@gmail.com>
2015-02-21 12:11:31 +0900

    config and README.md Edit. Ver.1.6.4

--- config.php
+++ config.php
@@ -14,7 +14,7 @@
   */
 
 $title = 'Nginxキャッシュクリア';
-$description = 'baserCMSのサーバキャッシュ削除とは別口で動作する、Nginx専用・キャッシュクリアプラグイン。- 事前に /etc/nginx/nginx.conf 内の ( proxy_cache_pass ~ ) へ記述されたキャッシュディレクトリ名の絶対パス ( /var/www/html 等のスラッシュから始まるパスの事。) 控えておき、Nginxキャッシュディレクトリ更新管理にて更新します。- ';
+$description = 'baserCMSのサーバキャッシュ削除とは別口で動作する、Nginx専用・キャッシュクリアプラグイン。- 事前に /etc/nginx/nginx.conf 内の ( proxy_cache_pass ~ ) へ記述されたキャッシュディレクトリ名の絶対パス ( /var/www/html 等のスラッシュから始まるパスの事。) を控えておき、Nginxキャッシュディレクトリ更新管理にて更新します。- ';
 $adminLink = array('plugin' => 'nginxcacheclear', 'controller' => 'nginxcacheclear', 'action' => 'index');
 $installMessage = 'Nginxキャッシュクリアプラグインをインストールしますか？';
 $author = 'hisanuco';
